1e0534d6a40f8d633a59bd78ff013e8b37e4196a
Cristian Rivera <cristian.rivera@gmail.com>
2021-04-14 17:53:07 -0400

    72. Comunicación entre componentes

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,23 +1,21 @@
 
 import React, {useState} from 'react'
+import { AddCategory } from './componets/AddCategory';
 
 export const GifExpertApp = () => {
 
-    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball'])
 
-    const handleAdd = () => {
-        // setCategories( [...categories, 'HunterXHunter'] );
-        setCategories(c => [...c, 'HounterXHounter'])
-    }
+    // const handleAdd = () => {
+    //     // setCategories( [...categories, 'HunterXHunter'] );
+    //     setCategories(c => [...c, 'HounterXHounter'])
+    // }
 
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
             <hr />
-
-            <button onClick={ handleAdd }>Agregar</button>
-
             <ol>
                 {
                     categories.map(category=> {
